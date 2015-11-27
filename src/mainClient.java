818fd25466a9f33b17d019c31a5ccebb2d301891
Buisson <colombet.aurelien@etu.unice.fr>
2015-11-27 11:02:19 +0100

    gestion de securité pour entrer le numero de port

--- src/mainClient.java
+++ src/mainClient.java
@@ -12,6 +12,7 @@ public class mainClient {
 		
 		System.out.println("##BEGIN TEST CLIENT");
 		Pattern pattern=Pattern.compile("([0-9]{1,3}\\.){3}[0-9]{1,3}"); //regexp IP
+		Pattern patternInt=Pattern.compile("[0-9]+"); //regexp IP
 	    
 		System.out.println("Veuillez rentrer l'IP du serveur (taper entrer pour avoir l'adresse IP par defaut 127.0.0.1)");
 		String ipConf = readUserCommand.nextLine();
@@ -29,7 +30,20 @@ public class mainClient {
 	    }
 	    int port=2000;
 	    System.out.println("Veuillez rentrer le numero de port (rentrer le numero port par defaut 2000)");
-		port = readUserCommand.nextInt();
+		String temp = readUserCommand.nextLine();
+		Matcher matcherint = patternInt.matcher(temp);
+		
+		while(!matcherint.matches()){
+			if(temp.isEmpty()){
+				break;
+			}
+			System.out.println("Veuillez entrer un numero de port valide");
+			temp = readUserCommand.next();
+			matcherint = patternInt.matcher(temp);
+		}
+		if(port!=2000)
+			port = Integer.parseInt(temp);
+		
 		readUserCommand = new Scanner(System.in);
 		
 	    Client c = new Client(ipConf,port);
